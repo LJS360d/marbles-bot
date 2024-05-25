9d21dfdcbee468bdf78a126b8a4265317ff9e5d9
LJS360d <luca.lencioni09@gmail.com>
2024-05-25 10:25:18 +0200

    update: cloudflared tunnel setup with discord

--- docker/cloudflared_url.sh
+++ docker/cloudflared_url.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
 source "$SCRIPT_DIR/.env"
 
 container=$COMPOSE_PROJECT_NAME-cloudflared
@@ -14,7 +13,27 @@ else
   exit 1
 fi
 
-api=https://discord.com/api/v9/applications/$DISCORD_APPLICATION_ID/proxy-config
+discord_login_api=https://discord.com/api/v9/auth/login
+RESPONSE=$(curl -s -X POST "$discord_login_api" \
+  -H "Content-Type: application/json" \
+  -d '{
+    "login": "'"$DISCORD_LOGIN"'",
+    "password": "'"$DISCORD_PASSWORD"'"
+  }')
+
+# ! DO NOT SHARE THIS TOKEN
+TOKEN=$(echo $RESPONSE | grep -oP '(?<="token":")[^"]*')
+
+if [ -n "$TOKEN" ]; then
+  echo "Discord Login Success"
+else
+  echo "Discord Login Failed"
+  exit 1
+fi
+
+echo "Application ID: $DISCORD_APPLICATION_ID"
+
+proxy_config_api=https://discord.com/api/v9/applications/$DISCORD_APPLICATION_ID/proxy-config
 payload=$(cat <<EOF
 {
   "url_map": [
@@ -27,5 +46,4 @@ payload=$(cat <<EOF
 EOF
 )
 
-# TODO change discord auth / add auth refresh somehow
-curl -X POST -H "Content-Type: application/json" -H "Authorization: $DISCORD_AUTH" -d "$payload" $api
+curl -X POST -H "Content-Type: application/json" -H "Authorization: $TOKEN" -d "$payload" $proxy_config_api
